f5a6f007a192b6aa6ca92ea20048bf29734fdbc6
Denis Stogniy <stogniy.denis@gmail.com>
2021-05-16 18:10:01 +0300

    initial commit

---
+++ NugetTestTemp/NugetClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NugetTestTemp
+{
+    public class NugetClass
+    {
+        public string SayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
